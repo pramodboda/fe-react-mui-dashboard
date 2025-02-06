acad62d08e3ba946ee42b05d12f95958f696586b
pramodboda <pramodkumarboda@gmail.com>
2025-02-06 20:32:32 +0530

    UPDATE: UI Enhancing

--- src/components/ProUI_Elements/Toasts/AutoLoadToastDrawer.tsx
+++ src/components/ProUI_Elements/Toasts/AutoLoadToastDrawer.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
-import { ToastContainer, toast, cssTransition } from 'react-toastify';
-
+import { ToastContainer, toast, cssTransition, ToastContentProps } from 'react-toastify';
+import Alert from '@mui/material/Alert';
 const AutoLoadToastDrawer = () => {
 
 
-  const swing = cssTransition({
-    enter: 'fade-in',
+  const slide = cssTransition({
+    enter: 'slide-in-blurred-top',
     exit:'slide-out-blurred-top'
   });
 
@@ -13,23 +13,33 @@ const AutoLoadToastDrawer = () => {
   useEffect(()=>{
   
 
-   
-    toast('Wow so easy !', {position: 'top-center', delay: 2500, transition:swing});
-    toast('Wow so easy !',{position: 'top-center', delay: 3000, transition:swing});
-    toast('Wow so easy !',{position: 'top-center', delay: 3500, transition:swing});
-    toast('Welcome User!',{
+    toast(Welcome,{
       position: 'top-center',
-      delay: 2000,
-      transition: swing
+      delay: 1000,
+      autoClose: 8000,
+      transition: slide
     });
+    toast('Wow so easy !', {position: 'top-center', delay: 2000, autoClose: 7000, transition:slide});
+    toast('Wow so easy !',{position: 'top-center', delay: 3000, autoClose: 6000, transition:slide});
+
  
   },[]);
 
   return (
-    <div className="grid place-items-center h-dvh bg-zinc-900/15">
-      <ToastContainer />
+    <div className="">
+      <ToastContainer newestOnTop/>
     </div>
   );
+
+
+  function Welcome({ closeToast }: ToastContentProps) {
+    return (
+   <Alert variant="filled" severity="success">
+  This is a filled success Alert.
+</Alert>
+    );
+
+    }
 };
 
 export default AutoLoadToastDrawer;
