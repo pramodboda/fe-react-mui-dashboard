327adaeca25c6604ad52f953b235f6fac2dc0db3
pramodboda <pramodkumarboda@gmail.com>
2025-03-26 21:36:18 +0530

    UPDATE: UI

--- src/pages/SitemapPage.tsx
+++ src/pages/SitemapPage.tsx
@@ -1,16 +1,25 @@
 
 import Grid from "@mui/material/Grid2";
 import {Box, Typography} from "@mui/material";
+
+import { NavLink } from "react-router-dom";
 // import { title } from "framer-motion/client";
 
 const sitemap_data = [{
+    title: "Dashboard",
+    subpages: ["DashboardHome1"]
+    },{
 title: "Pages",
 subpages: ["Buttons"]
 }]
 
 const SitemapPage: React.FC = () => {
-return <Box><Grid container spacing={2}>
-    {sitemap_data.map((i)=> {<Grid><Typography>{i.title}</Typography></Grid>})}
+return <Box>
+    <Typography variant="h6" sx={{textTransform: "Uppercase"}}>
+    Sitemap
+    </Typography>
+    <Grid container spacing={2}>
+    {sitemap_data.map((i)=> { return <Grid size={{xs:12, md:3}}><Typography >{i.title}</Typography> {i.subpages.map((sp)=>{return <NavLink to=''>{sb}</NavLink>})}</Grid>})}
     
     </Grid></Box>
 }
