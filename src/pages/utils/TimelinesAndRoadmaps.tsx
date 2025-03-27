f9e8c01d63425b623b4314a08fe67ba86ab67d88
pramodboda <pramodkumarboda@gmail.com>
2025-03-27 21:49:45 +0530

    UPDATE: UI

--- src/pages/utils/TimelinesAndRoadmaps.tsx
+++ src/pages/utils/TimelinesAndRoadmaps.tsx
@@ -1,4 +1,4 @@
-import Grid from "@mui/material/Grid";
+import Grid from "@mui/material/Grid2";
 
 import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
@@ -15,130 +15,130 @@ import Typography from '@mui/material/Typography';
 
 import ProUI_Card from '../../components/ProUI_Elements/ProUI_Card/ProUI_Card';
 
-const FE_React_Dev_Roadmap = [{skill_title:"HTML5",  position:"right"}, {skill_title:"CSS3", position:"right"}]
+const FE_React_Dev_Roadmap = [{ skill_title: "HTML5", position: "right" }, { skill_title: "CSS3", position: "right" }, { skill_title: "Boostrap", position: "right"}, { skill_title: "JavaScript", position: "right" }, { skill_title: "ES6+", position: "right" }, { skill_title: "TypeScript", position: "right" }, { skill_title: "React", position: "right" }]
 
-const TimelinesAndRoadmaps:React.FC = () => {
+const TimelinesAndRoadmaps: React.FC = () => {
     return <>
 
-<Grid container>
-    <Grid size={{sx:12, lg:3}}>
+        <Grid container>
+            <Grid size={{ xs: 12, lg: 3 }}>
 
-    <ProUI_Card>
+                <ProUI_Card>
 
-<Timeline position="alternate">
-{FE_React_Dev_Roadmap.map((item, index)=>{
-return <TimelineItem>
-<TimelineOppositeContent
-    key={index}
-  sx={{ m: 'auto 0' }}
-  align="right"
-  variant="body2"
-  color="text.secondary"
->
-  9:30 am
-</TimelineOppositeContent>
-<TimelineSeparator>
-  <TimelineConnector />
-  <TimelineDot>
-    <FastfoodIcon />
-  </TimelineDot>
-  <TimelineConnector />
-</TimelineSeparator>
-<TimelineContent sx={{ py: '12px', px: 2 }}>
-  <Typography variant="h6" component="span">
-    {item.skill_title}
-  </Typography>
-  <Typography>Because you need strength</Typography>
-</TimelineContent>
-</TimelineItem>
-})}
-  
+                    <Timeline position="alternate">
+                        {FE_React_Dev_Roadmap.map((item, index) => {
+                            return <TimelineItem>
+                                <TimelineOppositeContent
+                                    key={index}
+                                    sx={{ m: 'auto 0' }}
+                                    align="right"
+                                    variant="body2"
+                                    color="text.secondary"
+                                >
+                                    9:30 am
+                                </TimelineOppositeContent>
+                                <TimelineSeparator>
+                                    <TimelineConnector />
+                                    <TimelineDot>
+                                        <FastfoodIcon />
+                                    </TimelineDot>
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent sx={{ py: '12px', px: 2 }}>
+                                    <Typography variant="h6" component="span">
+                                        {item.skill_title}
+                                    </Typography>
+                                    <Typography>Because you need strength</Typography>
+                                </TimelineContent>
+                            </TimelineItem>
+                        })}
 
 
-  
-  <TimelineItem>
-    <TimelineOppositeContent
-      sx={{ m: 'auto 0' }}
-      align="right"
-      variant="body2"
-      color="text.secondary"
-    >
-      9:30 am
-    </TimelineOppositeContent>
-    <TimelineSeparator>
-      <TimelineConnector />
-      <TimelineDot>
-        <FastfoodIcon />
-      </TimelineDot>
-      <TimelineConnector />
-    </TimelineSeparator>
-    <TimelineContent sx={{ py: '12px', px: 2 }}>
-      <Typography variant="h6" component="span">
-        Eat
-      </Typography>
-      <Typography>Because you need strength</Typography>
-    </TimelineContent>
-  </TimelineItem>
-  <TimelineItem>
-    <TimelineOppositeContent
-      sx={{ m: 'auto 0' }}
-      variant="body2"
-      color="text.secondary"
-    >
-      10:00 am
-    </TimelineOppositeContent>
-    <TimelineSeparator>
-      <TimelineConnector />
-      <TimelineDot color="primary">
-        <LaptopMacIcon />
-      </TimelineDot>
-      <TimelineConnector />
-    </TimelineSeparator>
-    <TimelineContent sx={{ py: '12px', px: 2 }}>
-      <Typography variant="h6" component="span">
-        Code
-      </Typography>
-      <Typography>Because it&apos;s awesome!</Typography>
-    </TimelineContent>
-  </TimelineItem>
-  <TimelineItem>
-    <TimelineSeparator>
-      <TimelineConnector />
-      <TimelineDot color="primary" variant="outlined">
-        <HotelIcon />
-      </TimelineDot>
-      <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-    </TimelineSeparator>
-    <TimelineContent sx={{ py: '12px', px: 2 }}>
-      <Typography variant="h6" component="span">
-        Sleep
-      </Typography>
-      <Typography>Because you need rest</Typography>
-    </TimelineContent>
-  </TimelineItem>
-  <TimelineItem>
-    <TimelineSeparator>
-      <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-      <TimelineDot color="secondary">
-        <RepeatIcon />
-      </TimelineDot>
-      <TimelineConnector />
-    </TimelineSeparator>
-    <TimelineContent sx={{ py: '12px', px: 2 }}>
-      <Typography variant="h6" component="span">
-        Repeat
-      </Typography>
-      <Typography>Because this is the life you love!</Typography>
-    </TimelineContent>
-  </TimelineItem>
-</Timeline>
-</ProUI_Card>
-    </Grid>
-</Grid>
-  
 
-  
+
+                        <TimelineItem>
+                            <TimelineOppositeContent
+                                sx={{ m: 'auto 0' }}
+                                align="right"
+                                variant="body2"
+                                color="text.secondary"
+                            >
+                                9:30 am
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineConnector />
+                                <TimelineDot>
+                                    <FastfoodIcon />
+                                </TimelineDot>
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent sx={{ py: '12px', px: 2 }}>
+                                <Typography variant="h6" component="span">
+                                    Eat
+                                </Typography>
+                                <Typography>Because you need strength</Typography>
+                            </TimelineContent>
+                        </TimelineItem>
+                        <TimelineItem>
+                            <TimelineOppositeContent
+                                sx={{ m: 'auto 0' }}
+                                variant="body2"
+                                color="text.secondary"
+                            >
+                                10:00 am
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineConnector />
+                                <TimelineDot color="primary">
+                                    <LaptopMacIcon />
+                                </TimelineDot>
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent sx={{ py: '12px', px: 2 }}>
+                                <Typography variant="h6" component="span">
+                                    Code
+                                </Typography>
+                                <Typography>Because it&apos;s awesome!</Typography>
+                            </TimelineContent>
+                        </TimelineItem>
+                        <TimelineItem>
+                            <TimelineSeparator>
+                                <TimelineConnector />
+                                <TimelineDot color="primary" variant="outlined">
+                                    <HotelIcon />
+                                </TimelineDot>
+                                <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+                            </TimelineSeparator>
+                            <TimelineContent sx={{ py: '12px', px: 2 }}>
+                                <Typography variant="h6" component="span">
+                                    Sleep
+                                </Typography>
+                                <Typography>Because you need rest</Typography>
+                            </TimelineContent>
+                        </TimelineItem>
+                        <TimelineItem>
+                            <TimelineSeparator>
+                                <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+                                <TimelineDot color="secondary">
+                                    <RepeatIcon />
+                                </TimelineDot>
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent sx={{ py: '12px', px: 2 }}>
+                                <Typography variant="h6" component="span">
+                                    Repeat
+                                </Typography>
+                                <Typography>Because this is the life you love!</Typography>
+                            </TimelineContent>
+                        </TimelineItem>
+                    </Timeline>
+                </ProUI_Card>
+            </Grid>
+        </Grid>
+
+
+
     </>
-} 
+}
 
 export default TimelinesAndRoadmaps;
