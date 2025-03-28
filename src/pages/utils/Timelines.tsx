f67b80c6fd69d42266e9f04e6907b194ef295dbd
pramodboda <pramodkumarboda@gmail.com>
2025-03-28 18:07:06 +0530

    UPDATE: UI

--- src/pages/utils/Timelines.tsx
+++ src/pages/utils/Timelines.tsx
@@ -25,8 +25,11 @@ import { Description } from "@mui/icons-material";
 
 const FE_React_Dev_Roadmap = {
     coreFrontendSkills:[{ title: "HTML5", position: "right", icon:<FaHtml5/>, description:"(Semantic elements, Accessibility)" }, { title: "CSS3", position: "right", icon:<FaCss3 />, description:"(Flexbox, Grid, Animations, Preprocessors like SASS)" }, { title: "Boostrap", position: "right", icon: <SiBootstrap />,description:"()" }, { title: "JavaScript(ES6+)", position: "right", icon: <SiJavascript />,description:"(Closures, Hoisting, Promises, Async/Await)" }, { title: "ES6+", position: "right", icon: <BiSolidFileJs />,description:"()"  }, { title: "TypeScript", position: "right", icon: <SiTypescript/>,description:"(Optional, but preferred)"  }, { title: "React", position: "right", icon: <FaReact/>,description:"(Hooks, Context API, Lifecycle Methods, State Management)"  }, { title: "State Management", position: "right", icon: <SiRedux />,description:"(Redux, Recoil, Zustand, or Context API)"  }, { title: "React Router", position: "right", icon: <SiReactrouter />,description:"(Dynamic Routing, Nested Routes)"  }, { title: "React/ Component Design Patterns", position: "right", icon: <GiGearStickPattern/>,description:"(HOC, Render Props, Compound Components)" },{ title: "Frontend Performance Optimization", position: "right", icon: <IoSpeedometerOutline />,description:"(Lazy Loading, Code Splitting, Memoization)" }, { title: "Unit testing", position: "right", icon:<SiJest />,description:"(Jest, React Testing Library, Cypress)" }],
-
+    buildAndDeploymentTools:[{title:"Webpack / Vite", description:"(Tree Shaking, Code Splitting)"}, {title:"Babel", description:"(Transpiling Modern JS)"}, {title:"Package Managers", description:"(npm, yarn, pnpm)"}, {title:"CI/CD Tools", description:"(GitHub Actions, Jenkins)"}]
 }
+
+
+
 
 const Timelines: React.FC = () => {
     return <>
@@ -36,6 +39,41 @@ const Timelines: React.FC = () => {
                 <ProUI_Card>
                     <Timeline position="alternate">
                         {FE_React_Dev_Roadmap.coreFrontendSkills.map((skill, index) => {
+                            return <TimelineItem>
+                                <TimelineOppositeContent
+                                    key={index}
+                                    sx={{ m: 'auto 0' }}
+                                    align="right"
+                                    variant="body2"
+                                    color="text.secondary"
+                                >
+                                    9:30 am
+                                </TimelineOppositeContent>
+                                <TimelineSeparator>
+                                    <TimelineConnector />
+                                    <TimelineDot color="primary">
+                                    {skill.icon}
+                                    </TimelineDot>
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent sx={{ py: '12px', px: 2 }}>
+                                    <Typography variant="h6" component="span">
+                                        {skill.title}
+                                    </Typography>
+                                    <Typography>{skill.description}</Typography>
+                                </TimelineContent>
+                            </TimelineItem>
+                        })}
+
+
+                    </Timeline>
+                </ProUI_Card>
+            </Grid>
+
+            <Grid size={{ xs: 12, lg: 3 }}>
+                <ProUI_Card>
+                    <Timeline position="alternate">
+                        {FE_React_Dev_Roadmap.buildAndDeploymentTools.map((skill, index) => {
                             return <TimelineItem>
                                 <TimelineOppositeContent
                                     key={index}
