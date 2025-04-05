059ac35c1ff6d51770c22c88679c84d1c4425c39
pramodboda <pramodkumarboda@gmail.com>
2025-04-05 15:41:09 +0530

    UPDATE: UI

--- src/api/career-skills-data.ts
+++ src/api/career-skills-data.ts
@@ -2,6 +2,7 @@ const career_skills_data = [
 
     {
         title: "HTML and CSS: Design and Build Websites",
+        book_cover: "https://learning.oreilly.com/library/cover/9781118206911/250w/",
         author:"Jon Duckett",
         best_for: "Beginners to Intermediate",
         highlights: "Visually rich, full-color pages. Clear explanations of HTML and CSS concepts. Hands-on examples with clean design principles.",
@@ -10,6 +11,7 @@ const career_skills_data = [
 
     {
         title: "Learning Web Design (5th Edition)",
+        book_cover: "https://learning.oreilly.com/library/cover/9781118206911/250w/",
         author:"Jennifer Niederst Robbins",
         best_for: "Complete beginners",
         covers: "HTML, CSS, responsive design, and even a bit of JavaScript.",
@@ -19,6 +21,7 @@ const career_skills_data = [
 
     {
         title: "HTML5 and CSS3 All-in-One For Dummies",
+        book_cover: "https://learning.oreilly.com/library/cover/9781118206911/250w/",
         author:"Andy Harris",
         best_for: "Learners who want a solid foundation + extra skills",
         covers: "HTML5, CSS3, JavaScript basics, web programming.",
@@ -28,6 +31,7 @@ const career_skills_data = [
 
     {
         title: "Web Design with HTML, CSS, JavaScript and jQuery Set",
+        book_cover: "https://learning.oreilly.com/library/cover/9781118206911/250w/",
         author:"Jon Duckett",
         best_for: "People who want to go beyond HTML",
         covers: "Two books—one on HTML/CSS and one on JavaScript/jQuery.",
@@ -37,6 +41,7 @@ const career_skills_data = [
 
     {
         title: "HTML5: The Missing Manual",
+        book_cover: "https://learning.oreilly.com/library/cover/9781118206911/250w/",
         author:"Matthew MacDonald",
         best_for: "Intermediate users or those transitioning to HTML5",
         highlights: "Practical examples. Detailed explanation of new HTML5 tags and APIs.",
