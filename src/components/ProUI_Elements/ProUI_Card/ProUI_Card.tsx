9210a9dfd8611d7fccc28de11795ae0a24537ac6
Pramod Boda <PB493939@Haleon.ad>
2025-04-08 19:23:09 +0530

    UPDATE: UI

--- src/components/ProUI_Elements/ProUI_Card/ProUI_Card.tsx
+++ src/components/ProUI_Elements/ProUI_Card/ProUI_Card.tsx
@@ -1,4 +1,6 @@
-import { Box, Button, IconButton } from "@mui/material";
+import { Box, Button, Stack, IconButton, Typography } from "@mui/material";
+import CloseTwoToneIcon from "@mui/icons-material/CloseTwoTone";
+import DownloadTwoToneIcon from "@mui/icons-material/DownloadTwoTone";
 
 interface CustomCardProps {
   title?: string;
@@ -15,8 +17,32 @@ const ProUI_Card: React.FC<CustomCardProps> = ({
 }) => {
   return (
     <Box sx={{ backgroundColor: "#f9f9f9" }}>
-      {title && title}
-      {actions && <IconButton>A</IconButton>}
+      <Stack
+        sx={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "1rem ",
+        }}
+      >
+        <Box sx={{ width: "70%" }}>
+          {" "}
+          {title && <Typography>{title}</Typography>}
+        </Box>
+        <Box>
+          {actions && (
+            <>
+              <IconButton>
+                <CloseTwoToneIcon />
+              </IconButton>
+              <IconButton>
+                <DownloadTwoToneIcon />
+              </IconButton>
+            </>
+          )}
+        </Box>
+      </Stack>
+
       <Box sx={{ padding: "25px", backgroundColor: "#f9f9f9" }}>{children}</Box>
     </Box>
   );
