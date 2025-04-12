fdde30f8baff560b71437dbbc1342d7845241d4f
Pramod Boda <pramodboda@Pramods-iMac.local>
2025-04-13 04:18:51 +0530

    UPDATE: UI

--- src/components/ProUI_Elements/Typography/ProUI_Typography.tsx
+++ src/components/ProUI_Elements/Typography/ProUI_Typography.tsx
@@ -5,52 +5,55 @@ import Typography from "@mui/material/Typography";
 export default function ProUI_Typography() {
   return (
     <Box sx={{ width: "100%" }}>
+      <Typography gutterBottom sx={{ fontSize: "6.25rem", fontWeight: 700 }}>
+        Jumbo Heading with 6.25rem (100px)
+      </Typography>
       <Typography variant="h1" gutterBottom>
-        h1. Heading with 6rem (96px)
+        h1. Heading with 3rem (48px)
       </Typography>
       <Typography variant="h2" gutterBottom>
-        h2. Heading with 3.75rem (60px)
+        h2. Heading with 2.25rem (36px)
       </Typography>
       <Typography variant="h3" gutterBottom>
-        h3. Heading with 3rem (48px)
+        h3. Heading with 1.75rem (28px)
       </Typography>
       <Typography variant="h4" gutterBottom>
-        h4. Heading with 2.125rem (34px)
+        h4. Heading with 1.5rem (24px)
       </Typography>
       <Typography variant="h5" gutterBottom>
-        h5. Heading with 1.5rem (24px)
+        h5. Heading with 1.25rem (20px)
       </Typography>
       <Typography variant="h6" gutterBottom>
-        h6. Heading with 1.25rem (20px)
+        h6. Heading with 1rem (16px)
       </Typography>
       <Typography variant="subtitle1" gutterBottom>
-        subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Quos blanditiis tenetur
+        subtitle1 with "0.875rem" (14px). Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Quos blanditiis tenetur
       </Typography>
       <Typography variant="subtitle2" gutterBottom>
-        subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Quos blanditiis tenetur
+        subtitle2 with "0.75rem" (12px). Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Quos blanditiis tenetur
       </Typography>
       <Typography variant="body1" gutterBottom>
-        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur unde suscipit, quam beatae rerum inventore
-        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-        fugiat deleniti? Eum quasi quidem quibusdam.
+        body1 with "1rem" (16px). Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae
+        rerum inventore consectetur, neque doloribus, cupiditate numquam
+        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
       </Typography>
       <Typography variant="body2" gutterBottom>
-        body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur unde suscipit, quam beatae rerum inventore
-        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-        fugiat deleniti? Eum quasi quidem quibusdam.
+        body2 "0.875rem" (14px). Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae
+        rerum inventore consectetur, neque doloribus, cupiditate numquam
+        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
       </Typography>
       <Typography variant="button" gutterBottom sx={{ display: "block" }}>
-        button text
+        button text with "0.875rem" (14px)
       </Typography>
       <Typography variant="caption" gutterBottom sx={{ display: "block" }}>
-        caption text
+        caption text with "0.75rem" (12px)
       </Typography>
       <Typography variant="overline" gutterBottom sx={{ display: "block" }}>
-        overline text
+        overline text with "0.625rem", (10px)
       </Typography>
     </Box>
   );
