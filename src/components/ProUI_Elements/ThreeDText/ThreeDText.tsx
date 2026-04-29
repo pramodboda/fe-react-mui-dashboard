b6fbe9e3c9a6df9a2e03c400e4db5dfb7899f5dd
pramodboda <pramodkumarboda@gmail.com>
2026-04-29 21:29:09 +0530

    UI Updates

--- src/components/ProUI_Elements/ThreeDText/ThreeDText.tsx
+++ src/components/ProUI_Elements/ThreeDText/ThreeDText.tsx
@@ -5,7 +5,7 @@ import Box from "@mui/material/Box";
 export default function ThreeDText({text}){
 
     return(
-        <Box sx={{height: "50vh", position: "relative"}}>
+        <Box sx={{height: "80vh", position: "relative"}}>
         <span className="threeD_text">{text?text:"Your text here"}</span>
         </Box>)
     
