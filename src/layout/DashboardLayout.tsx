f0561c27502f8641b9830d8ba697e1b2873f0fa3
pramodboda <pramodkumarboda@gmail.com>
2026-04-29 23:14:50 +0530

    UI Updates

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -365,7 +365,8 @@ export default function DashboardLayout() {
                   variant="h6"
                   noWrap
                   component="div"
-                  sx={{ flexGrow: 1 }}
+                  sx={{ flexGrow: 1, fontSize: "1.5rem", fontFamily: "Warnes, cursive",
+                  fontWeight: 600 }}
                 >
                   CodeVik
                 </Typography>
@@ -490,22 +491,6 @@ export default function DashboardLayout() {
               <DrawerHeader />
 
               <Outlet />
-              <Typography variant="body2" sx={{ marginBottom: 2 }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Rhoncus dolor purus non enim praesent elementum facilisis leo
-                vel. Risus at ultrices mi tempus imperdiet. Semper risus in
-                hendrerit gravida rutrum quisque non tellus. Convallis convallis
-                tellus id interdum velit laoreet id donec ultrices. Odio morbi
-                quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                adipiscing bibendum est ultricies integer quis. Cursus euismod
-                quis viverra nibh cras. Metus vulputate eu scelerisque felis
-                imperdiet proin fermentum leo. Mauris commodo quis imperdiet
-                massa tincidunt. Cras tincidunt lobortis feugiat vivamus at
-                augue. At augue eget arcu dictum varius duis at consectetur
-                lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                sapien faucibus et molestie ac.
-              </Typography>
             </Box>
             <Footer_1 />
           </Box>
